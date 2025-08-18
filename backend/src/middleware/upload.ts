80aac3e6179586287500eea64dff362c190e2534
RTSaitama <rtsaitama@outlook.com>
2025-08-18 12:09:06 +0300

    readme fix

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -16,7 +16,7 @@ const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
   if (file.mimetype.startsWith('image/')) {
     cb(null, true);
   } else {
-    cb(new Error('Only image files are allowed!'), false);
+    cb(new Error('Only images are allowed!'), false);
   }
 };
 
